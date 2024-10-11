19dad4416bdc49a0c21415ebc46b2872addd8632
Yunsu4 <ysa020428@gmail.com>
2024-10-11 09:37:44 +0900

    hide access id and key

--- s3_test.py
+++ s3_test.py
@@ -7,8 +7,8 @@ def s3_connection():
         s3 = boto3.client(
             service_name ="s3",
             region_name= "ap-northeast-2",
-            aws_access_key_id="AKIAQWHCPWQYQYSKKJPK",
-            aws_secret_access_key="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",
+            aws_access_key_id="[[access_id]]",
+            aws_secret_access_key="[[access_key]]",
         )
     except Exception as e:
         print(e)
